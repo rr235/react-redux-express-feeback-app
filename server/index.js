d5db5006393c88bf393747bdcc62ffb1175d405e
RahulR <rash235r@hotmail.com>
2018-05-18 02:05:22 +0400

    Sparkle some colors 🌈

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
+const chalk = require("chalk");
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send({ hi: "there" });
 });
 
-app.listen(5000);
-console.log("listening at 5000");
+app.listen(PORT);
+console.log(chalk.bold.cyan("listening at " + PORT));
