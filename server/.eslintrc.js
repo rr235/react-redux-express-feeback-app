1d100e5a28c6252cc776a8c07e214ffe86602369
RahulR <rash235r@hotmail.com>
2018-05-19 19:42:31 +0400

    Setup Prettier 🦋 and Eslint 🐠

---
+++ server/.eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  extends: ['airbnb', 'prettier'], // extending recommended config and config derived from eslint-config-prettier
+  plugins: ['prettier'], // activating esling-plugin-prettier (--fix stuff)
+  rules: {
+    'prettier/prettier': [
+      // customizing prettier rules
+      'error',
+      {
+        singleQuote: true,
+        trailingComma: 'all',
+      },
+    ],
+    eqeqeq: ['error', 'always'], // adding some custom ESLint rules
+  },
+};
--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
-const express = require("express");
+const express = require('express');
+const chalk = require('chalk');
+
 const app = express();
-const chalk = require("chalk");
+
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send({ hi: "there" });
+app.get('/', (req, res) => {
+  res.send({ hi: 'there' });
 });
 
 app.listen(PORT);
-console.log(chalk.bold.cyan("listening at " + PORT));
+console.log(chalk.bold.cyan(`listening at ${PORT}`)); // eslint-disable-line
