8dcf6df8d3425a757428b481096621d874f2e9dd
RahulR <rash235r@hotmail.com>
2018-05-27 22:10:42 +0400

    Set up google auth using Passport 📇 and set up nodemon 🔦

--- server/index.js
+++ server/index.js
@@ -1,13 +1,35 @@
 const express = require('express');
 const chalk = require('chalk');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
 const app = express();
 
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log('access token - ', accessToken);
+      console.log('refresh token - ', refreshToken);
+      console.log('profile - ', profile);
+    }
+  )
+);
+
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'));
+
 const PORT = process.env.PORT || 5000;
-
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
-});
-
 app.listen(PORT);
 console.log(chalk.bold.cyan(`listening at ${PORT}`)); // eslint-disable-line
