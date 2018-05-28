f7c09d46c075334939cb2eab22f2ab72f840c6f2
RahulR <rash235r@hotmail.com>
2018-05-28 18:56:30 +0400

    Little bit of Refactoring 🗄

--- server/index.js
+++ server/index.js
@@ -1,35 +1,11 @@
 const express = require('express');
 const chalk = require('chalk');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./config/keys');
+require('./services/passport');
 
 const app = express();
 
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback'
-    },
-    (accessToken, refreshToken, profile, done) => {
-      console.log('access token - ', accessToken);
-      console.log('refresh token - ', refreshToken);
-      console.log('profile - ', profile);
-    }
-  )
-);
-
-app.get(
-  '/auth/google',
-  passport.authenticate('google', {
-    scope: ['profile', 'email']
-  })
-);
-
-app.get('/auth/google/callback', passport.authenticate('google'));
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
-console.log(chalk.bold.cyan(`listening at ${PORT}`)); // eslint-disable-line
+console.log(chalk.bold.cyan(`Listening at ${PORT}`)); // eslint-disable-line
