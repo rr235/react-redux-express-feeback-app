ded5cdff6538a9cdbe4d03fd6c72b63960a84a53
RahulR <rash235r@hotmail.com>
2018-06-02 21:56:48 +0400

    Basic redux setup 🔌

---
+++ server/client/src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => (
+  <div>
+    Grab a Beer{' '}
+    <span role="img" aria-label="Beer!">
+      🍺
+    </span>
+  </div>
+);
+
+export default App;
