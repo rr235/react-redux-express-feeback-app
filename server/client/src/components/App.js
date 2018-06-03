ef7205c4a22a3a616ccc65e5b183f31c401f3627
RahulR <rash235r@hotmail.com>
2018-06-03 19:05:08 +0400

    Set up Routes with dummy components 🛣

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const Header = () => <h1>Header</h1>;
+const Dashboard = () => <h2>Dashboard</h2>;
+const Surveynew = () => <h2>SurveyNew</h2>;
+const Landing = () => <h2>Landing</h2>;
 
 const App = () => (
   <div>
-    Grab a Beer{' '}
-    <span role="img" aria-label="Beer!">
-      🍺
-    </span>
+    <BrowserRouter>
+      <div>
+        <Header />
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/surveys" component={Dashboard} />
+        <Route path="/surveys/new" component={Surveynew} />
+      </div>
+    </BrowserRouter>
   </div>
 );
 
