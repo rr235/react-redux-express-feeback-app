e036aee31b5fb3855e17dd1e2e0af848aa82e500
RahulR <rash235r@hotmail.com>
2018-06-05 19:40:49 +0400

    + ❤️ Arrow functions 🏹

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -1,7 +1,5 @@
 import axios from 'axios';
 import { FETCH_USER } from './types';
 
-export const fetchUser = () => async dispatch => {
-  const res = await axios.get('/api/current_user');
-  dispatch({ type: FETCH_USER, payload: res });
-};
+export const fetchUser = () => async dispatch =>
+  dispatch({ type: FETCH_USER, payload: await axios.get('/api/current_user') });
