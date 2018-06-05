498cfd145d122a0a9cb823c214f7d05f0f4a59ea
RahulR <rash235r@hotmail.com>
2018-06-05 21:47:10 +0400

    Change header based on user login status

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -1,6 +1,26 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component {
+  renderContent = () => {
+    switch (this.props.auth) {
+      case null:
+        return; // show nothing
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">Login with google</a>
+          </li>
+        );
+      default:
+        return (
+          <li>
+            <a href="">Logout</a>
+          </li>
+        );
+    }
+  };
+
   render() {
     return (
       <nav>
@@ -8,15 +28,13 @@ class Header extends Component {
           <a href="" className="left brand-logo">
             Emaily
           </a>
-          <ul className="right">
-            <li>
-              <a href="">Login with google</a>
-            </li>
-          </ul>
+          <ul className="right">{this.renderContent()}</ul>
         </div>
       </nav>
     );
   }
 }
 
-export default Header;
+const mapStateToProps = state => ({ auth: state.auth });
+
+export default connect(mapStateToProps)(Header);
