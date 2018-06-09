5ad98e16f74b86d1ac62e136a2f9e5fd24f29e77
RahulR <rash235r@hotmail.com>
2018-06-09 21:43:54 +0400

    Setup to serve build files while in production mode

--- server/index.js
+++ server/index.js
@@ -32,6 +32,17 @@ require('./routes/authRoutes')(app);
 
 require('./routes/billingRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+  // to serve production assets (css and js)
+  app.use(express.static('client/build'));
+
+  // serve index.html if the request doesnt match any of the defined route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 // listen for connections
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
