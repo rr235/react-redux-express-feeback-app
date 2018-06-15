8448f808e6ec5aa06d9126daf78c353e405351f0
RahulR <rash235r@hotmail.com>
2018-06-15 10:28:47 +0400

    Added Redirect Domain for email

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -10,7 +10,11 @@ const Survey = mongoose.model('surveys');
 // Hence, this is a workaround to prevent that from happening.
 
 module.exports = app => {
-  app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
+  app.get('/api/surveys/thanks', (req, res) => {
+    res.send('Thanks for voting!');
+  });
+
+  app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
     const { title, subject, body, recipients } = req.body;
     const survey = new Survey({
       title,
@@ -22,6 +26,16 @@ module.exports = app => {
     });
 
     const mailer = new Mailer(survey, surveyTemplate(survey));
-    mailer.send();
+
+    try {
+      await mailer.send();
+      await survey.save();
+
+      req.user.credits -= 1;
+      const user = await req.user.save();
+      res.send(user);
+    } catch (error) {
+      res.status(422).send(error);
+    }
   });
 };
