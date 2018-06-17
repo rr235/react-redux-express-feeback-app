b7a3b1596f5d505a4b861da976703a53ce4dfacd
RahulR <rash235r@hotmail.com>
2018-06-17 11:09:59 +0400

    Logic to update the responded recipient 🎩

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -43,7 +43,6 @@ module.exports = app => {
   });
 
   app.post('/api/surveys/webhooks', (req, res) => {
-    console.log(req.body);
     const p = new Path('/api/surveys/:surveyId/:choice');
 
     const events = _.chain(req.body)
@@ -55,9 +54,22 @@ module.exports = app => {
       })
       .compact()
       .uniqBy('email', 'surveyId')
+      .each(({ surveyId, email, choice }) => {
+        Survey.updateOne(
+          {
+            _id: surveyId,
+            recipients: {
+              $elemMatch: { email, responded: false }
+            }
+          },
+          {
+            $inc: { [choice]: 1 },
+            $set: { 'recipients.$.responded': true },
+            lastResponded: new Date()
+          }
+        ).exec();
+      })
       .value();
-
-    console.log(events);
 
     res.send({});
   });
